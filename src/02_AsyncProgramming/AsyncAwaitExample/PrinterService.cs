9d10fc4e45b3f276afa9cb2394796466ecb1e885
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-20 13:13:19 +0200

    Wywołanie operacji asynchronicznej w konstruktorze

---
+++ src/02_AsyncProgramming/AsyncAwaitExample/PrinterService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitExample;
+
+internal class PrinterService
+{
+    private readonly Printer printer;
+
+    public PrinterService(Printer printer)
+    {
+        this.printer = printer;
+    }
+
+    public async Task PrintAndCalculateAsync()
+    {
+        CostCalculator calculator = new CostCalculator();
+
+        // var printer = lazyPrinter.Value;
+
+        // Asynchroniczny z async-await
+        await printer.PrintAsync("Document #1");
+        decimal cost2 = await calculator.CalculateAsync("Document #1");
+        Console.WriteLine($"Cost: {cost2:C2}");
+    }
+}
--- src/02_AsyncProgramming/AsyncAwaitExample/Program.cs
+++ src/02_AsyncProgramming/AsyncAwaitExample/Program.cs
@@ -1,11 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AsyncAwaitExample;
+using Microsoft.Extensions.DependencyInjection;
 
 Console.WriteLine("Hello, Tasks!");
 
+async Task<Printer> CreateAsync()
+{
+    var printer = new Printer();
+    await printer.InitAsync();
+    return printer;
+}
+
 Printer printer = new Printer();
 CostCalculator calculator = new CostCalculator();
-
 
 // Synchroniczny
 printer.Print("Document #1");
@@ -14,8 +21,23 @@ Console.WriteLine($"Cost: {cost:C2}");
 
 // Asynchroniczny
 printer.PrintAsync("Document #1")
-    .ContinueWith(t => calculator.CalculateAsync("Document #1")
-        .ContinueWith(task => Console.WriteLine($"Cost: {task.Result:C2}")));
+    .ContinueWith(taskPrint => calculator.CalculateAsync("Document #1")
+        .ContinueWith(calculateTask => Console.WriteLine($"Cost: {calculateTask.Result:C2}")));
+
+
+// Microsoft.Extensions.DependencyInjection 
+var serviceProvider = new ServiceCollection()
+          .AddSingleton<PrinterService>()
+          .AddSingleton<Printer>(await CreateAsync())
+          .BuildServiceProvider();
+
+
+PrinterService printerService = serviceProvider.GetService<PrinterService>();
+
+
+// PrinterService printerService = new PrinterService();
+
+await printerService.PrintAndCalculateAsync();
 
 
 Console.WriteLine("Finished.");
@@ -26,6 +48,16 @@ Console.ReadLine();
 
 public class Printer
 {
+    public Task InitAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public Printer()
+    {
+        Console.WriteLine("Printer initalized.");
+    }
+
     public void Print(string content)
     {
         Console.WriteLine($"Printing '{content}' on thread {Thread.CurrentThread.ManagedThreadId}...");
