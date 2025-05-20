5248c468f57c3c1ae8118d9251b3e13f8810f6bc
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-20 10:09:37 +0200

    Dodanie synchronicznego pobierania zdjęć

--- src/01_Threads/ThreadExample/Program.cs
+++ src/01_Threads/ThreadExample/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, Thread!");
+
+for (int i = 0; i < 5; i++)
+{
+    DownloadImage(i);
+}
+
+Console.WriteLine($"[Thread] Thread# {Thread.CurrentThread.ManagedThreadId} finished.");
+
+
+
+static void DownloadImage(int index)
+{
+    Console.WriteLine($"[Thread] Thread# {Thread.CurrentThread.ManagedThreadId} image #{index}... downloading ");
+
+    using var client = new HttpClient();
+    var bytes = client.GetByteArrayAsync("https://picsum.photos/200").Result;
+    File.WriteAllBytes($"image{index}.jpg", bytes);
+
+    Console.WriteLine($"[Thread] Thread# {Thread.CurrentThread.ManagedThreadId} #{index} downloaded.");
+}
