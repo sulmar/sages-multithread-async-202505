d429de2a3b75773b4a4c7d586f9ebf17d965aa67
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-20 14:21:02 +0200

    Dodanie drukowania na fragmenty

--- src/02_AsyncProgramming/CancellationTokenExample/Program.cs
+++ src/02_AsyncProgramming/CancellationTokenExample/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Threading.Tasks;
+
+Console.WriteLine("Hello, Cancellation Token!");
+
+Printer printer = new Printer();
+await printer.PrintAsync("Document #1");
+
+Console.WriteLine("Press Enter to exit.");
+Console.ReadLine();
+
+
+
+class Printer
+{
+
+    public async Task PrintAsync(string content)
+    {
+        Console.WriteLine($"Printing '{content}' on thread {Thread.CurrentThread.ManagedThreadId}...");
+
+        for (int i = 0; i < 5; i++)
+        {
+            await Task.Delay(200);
+
+            Console.WriteLine($"...printing chunk {i + 1}"); // Drukowanie fragmentu strony
+
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+        }
+
+        Console.WriteLine($"Print completed.");
+    }
+}
