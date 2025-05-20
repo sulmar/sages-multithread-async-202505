1a2b1bf4a5d65668acb8b80745dfb270acd38386
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-20 11:53:03 +0200

    Dodanie drukowania synchronicznego

--- src/02_AsyncProgramming/AsyncAwaitExample/Program.cs
+++ src/02_AsyncProgramming/AsyncAwaitExample/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Tasks!");
+
+Printer printer = new Printer();
+
+printer.Print("Document #1");
+printer.Print("Document #2");
+printer.Print("Document #3");
+
+Console.WriteLine("Finished.");
+Console.ReadLine();
+
+
+public class Printer
+{
+    public void Print(string content)
+    {
+        Console.WriteLine($"Printing '{content}' on thread {Thread.CurrentThread.ManagedThreadId}...");
+        Thread.Sleep(TimeSpan.FromSeconds(1));
+        Console.WriteLine($"Print completed.");
+    }
+}
