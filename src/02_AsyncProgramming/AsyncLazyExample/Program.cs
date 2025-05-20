a18c2ae891c2bdb28c35864cb1db617528edd9af
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-20 16:03:58 +0200

    Dodanie wzorca AsyncLazy

--- src/02_AsyncProgramming/AsyncLazyExample/Program.cs
+++ src/02_AsyncProgramming/AsyncLazyExample/Program.cs
@@ -1,2 +1,81 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+PrinterService service = new PrinterService();
+await service.PrintAndCalculateAsync();
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+
+// Podobnie jak Lazy<T>, ale działa z Task<T> - inicjalizacja wykonywana jest asynchronicznie tylko raz
+// Użyteczne, gdy chcesz np. leniwie zainicjalizować połączenie do zdalnej bazy danych lub konfigurację pobieraną z sieci
+
+public class AsyncLazy<T>
+{
+    private readonly Lazy<Task<T>> _lazyTask;
+
+    public AsyncLazy(Func<Task<T>> factory)
+    {
+        _lazyTask = new Lazy<Task<T>>(factory);
+    }
+
+    public AsyncLazy(Lazy<Task<T>> lazyTask)
+    {
+        _lazyTask = lazyTask;
+    }
+
+    public Task<T> Value => _lazyTask.Value;
+}
+
+
+public class PrinterService
+{
+    private readonly AsyncLazy<Printer> lazyPrinter;
+
+    public PrinterService()
+    {
+        this.lazyPrinter = new AsyncLazy<Printer>(CreateAsync);
+    }
+
+    private async Task<Printer> CreateAsync()
+    {
+        var printer = new Printer();
+        await printer.InitAsync();
+
+        return printer;
+    }
+
+    public async Task PrintAndCalculateAsync()
+    {
+        var printer = await GetPrinterAsync();
+
+        await printer.PrintAsync("Document #1");
+    }
+
+    public Task<Printer> GetPrinterAsync() => lazyPrinter.Value;
+}
+
+public class Printer
+{
+    public Task InitAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public Printer()
+    {
+        Console.WriteLine("Printer initalized.");
+    }
+
+    public void Print(string content)
+    {
+        Console.WriteLine($"Printing '{content}' on thread {Thread.CurrentThread.ManagedThreadId}...");
+        Thread.Sleep(TimeSpan.FromSeconds(1));
+        Console.WriteLine($"Print completed.");
+    }
+
+    public Task PrintAsync(string content)
+    {
+        return Task.Run(() => Print(content));
+    }
+}
