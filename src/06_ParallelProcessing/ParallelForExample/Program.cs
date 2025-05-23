e54c5bd004ff903e7dbf5da452bb6172d2c7a16b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-23 13:06:15 +0200

    Dodanie przetwarzania równogłego za pomocą Tasks

--- src/06_ParallelProcessing/ParallelForExample/Program.cs
+++ src/06_ParallelProcessing/ParallelForExample/Program.cs
@@ -1,19 +1,42 @@
 ﻿
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 Console.WriteLine("Hello, Parallel.For!");
 
-const int items = 20;
+const int count = 20;
 
-MeasureForExecutionTime(items);
+MeasureForExecutionTime(count);
+MeasureForExecutionTimeTask(count);
 
-void MeasureForExecutionTime(int items)
+Console.ReadKey();
+
+async Task MeasureForExecutionTimeTask(int count)
+{
+    Console.WriteLine("Wykonanie asynchroniczne...");
+    
+    var stopwatch = Stopwatch.StartNew();
+
+    var items = Enumerable.Range(0, count);
+
+    var tasks = items.Select(i => DoWorkAsync(i));
+    await Task.WhenAll(tasks);
+
+    stopwatch.Stop();
+
+
+    Console.WriteLine($"Czas wykonania (taski): {stopwatch.ElapsedMilliseconds} ms");
+}
+
+
+
+void MeasureForExecutionTime(int count)
 {
     Console.WriteLine("Wykonanie sekwencyjne...");
 
     var stopwatch = Stopwatch.StartNew();
 
-    for (int i = 0; i < items; i++)
+    for (int i = 0; i < count; i++)
     {
         DoWork(i);
     }
@@ -24,10 +47,19 @@ void MeasureForExecutionTime(int items)
     Console.WriteLine($"Czas wykonania (sekwencyjnie): {stopwatch.ElapsedMilliseconds} ms");
 }
 
+static async Task DoWorkAsync(int item)
+{
+    Console.WriteLine($"Przetwarzanie {item} na wątku {Thread.CurrentThread.ManagedThreadId}");
+    Thread.SpinWait(1_000_000); // Obciążenie CPU
+    
+    await Task.Delay(100); // Symulacja opoźnienia
+    Console.WriteLine($"Zakonczono {item} na wątku {Thread.CurrentThread.ManagedThreadId}");
+}
 // Symulacja operacji CPU-bound czasochłonnej
 static void DoWork(int item)
 {
     Console.WriteLine($"Przetwarzanie {item} na wątku {Thread.CurrentThread.ManagedThreadId}");
     Thread.SpinWait(1_000_000); // Obciążenie CPU
     Thread.Sleep(100); // Symulacja opoźnienia
+    Console.WriteLine($"Zakonczono {item} na wątku {Thread.CurrentThread.ManagedThreadId}");
 }
