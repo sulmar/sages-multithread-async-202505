d29e2ad0f65bb31492799098c8c6190ed2bb4ed0
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-23 07:05:01 +0200

    Dodanie przykładu Parallel.For

--- src/06_ParallelProcessing/ParallelForExample/Program.cs
+++ src/06_ParallelProcessing/ParallelForExample/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Diagnostics;
+
+Console.WriteLine("Hello, Parallel.For!");
+
+const int items = 20;
+
+MeasureForExecutionTime(items);
+
+void MeasureForExecutionTime(int items)
+{
+    Console.WriteLine("Start Executing...");
+
+    var stopwatch = Stopwatch.StartNew();
+
+    for (int i = 0; i < items; i++)
+    {
+        DoWork(i);
+    }
+
+    stopwatch.Stop();
+
+
+    Console.WriteLine($"Time elapsed: {stopwatch.ElapsedMilliseconds} ms");
+}
+
+// Symulacja operacji CPU-bound czasochłonnej
+static void DoWork(int i)
+{
+    Thread.SpinWait(1_000_000); // Obciążenie CPU
+    Thread.Sleep(100); // Symulacja opoźnienia
+}
