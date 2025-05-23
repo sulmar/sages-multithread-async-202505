6b30c5c33036b9ab2c4618f10ea785ed8c4d573b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-23 08:17:07 +0200

    Dodanie Parallel.Invoke

--- src/06_ParallelProcessing/ParallelInvokeExample/Program.cs
+++ src/06_ParallelProcessing/ParallelInvokeExample/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, Parallel.Invoke!");
+Console.WriteLine($"CPU: {Environment.ProcessorCount} rdzeni");
+
+var items = Enumerable.Range(1, 20);
+
+// Tworzymy listę akcji do wykonania po kolei
+var actions = items
+    .Select(item => (Action)(() => ProcessItem(item)))
+    .ToList();
+
+// Sekwencyjnie wykonujemy wszystkie akcje
+foreach (var action in actions)
+{
+    action();
+}
+
+static void ProcessItem(int item)
+{
+    Console.WriteLine($"Start {item} na wątku {Thread.CurrentThread.ManagedThreadId}");
+
+    Thread.SpinWait(1_000_000); // CPU-bound
+
+    Thread.Sleep(500); // Symulacja I/O lub opóźnienia (blokująca)
+
+    Console.WriteLine($"Zakończono {item} na wątku {Thread.CurrentThread.ManagedThreadId}");
+}
