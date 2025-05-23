5e7be438f368e803543d8c5a19f7e13cd39a66f9
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-23 14:52:55 +0200

    Dodanie Parallel.Invoke

--- src/06_ParallelProcessing/ParallelInvokeExample/Program.cs
+++ src/06_ParallelProcessing/ParallelInvokeExample/Program.cs
@@ -1,26 +1,43 @@
 ﻿Console.WriteLine("Hello, Parallel.Invoke!");
 Console.WriteLine($"CPU: {Environment.ProcessorCount} rdzeni");
 
-var items = Enumerable.Range(1, 20);
+Parallel.Invoke(
+    () => ProcessItem(1), 
+    () => DoWork(1),
+    () => ThirdAction(199));
 
-// Tworzymy listę akcji do wykonania po kolei
-var actions = items
-    .Select(item => (Action)(() => ProcessItem(item)))
-    .ToList();
 
-// Sekwencyjnie wykonujemy wszystkie akcje
-foreach (var action in actions)
-{
-    action();
-}
 
 static void ProcessItem(int item)
 {
-    Console.WriteLine($"Start {item} na wątku {Thread.CurrentThread.ManagedThreadId}");
+    Console.WriteLine($"ProcessItem Start {item} na wątku {Thread.CurrentThread.ManagedThreadId}");
 
     Thread.SpinWait(1_000_000); // CPU-bound
 
     Thread.Sleep(500); // Symulacja I/O lub opóźnienia (blokująca)
 
-    Console.WriteLine($"Zakończono {item} na wątku {Thread.CurrentThread.ManagedThreadId}");
+    Console.WriteLine($"ProcessItem Zakończono {item} na wątku {Thread.CurrentThread.ManagedThreadId}");
+}
+
+
+static void DoWork(int item)
+{
+    Console.WriteLine($"DoWork Start {item} na wątku {Thread.CurrentThread.ManagedThreadId}");
+
+    Thread.SpinWait(1_000_000); // CPU-bound
+
+    Thread.Sleep(500); // Symulacja I/O lub opóźnienia (blokująca)
+
+    Console.WriteLine($"DoWork Zakończono {item} na wątku {Thread.CurrentThread.ManagedThreadId}");
+}
+
+static void ThirdAction(int item)
+{
+    Console.WriteLine($"ThirdAction Start {item} na wątku {Thread.CurrentThread.ManagedThreadId}");
+
+    Thread.SpinWait(1_000_000); // CPU-bound
+
+    Thread.Sleep(500); // Symulacja I/O lub opóźnienia (blokująca)
+
+    Console.WriteLine($"ThirdAction Zakończono {item} na wątku {Thread.CurrentThread.ManagedThreadId}");
 }
