35a7d730957aa7be257dc6ef9a3b33b3d0f2f10f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-23 11:10:39 +0200

    Dodanie sygnału zakończenia księgowania

--- src/03_ConcurrentCollections/BlockingCollectionExample/Program.cs
+++ src/03_ConcurrentCollections/BlockingCollectionExample/Program.cs
@@ -10,16 +10,28 @@ Task.Run(() => processor.Process());
 // Symulacja generowania faktur do zaksięgowania
 for (int i = 0; i < 10; i++)
 {
-    var invoice = new Invoice { Number = $"Invoice #{i}", TotalAmount = Math.Round( Random.Shared.NextDouble() * 1000 + 100, 2) };
+    var invoice = new Invoice { Number = $"Invoice #{i}", TotalAmount = Math.Round(Random.Shared.NextDouble() * 1000 + 100, 2) };
 
     processor.Accounting(invoice);
 
     Thread.Sleep(2000);
 }
 
+processor.Complete();
 
+Thread.Sleep(1000);
 
+try
+{
+    processor.Accounting(new Invoice { Number = "001", TotalAmount = 1.99d });
 
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+Console.WriteLine("Finished.");
 Console.ReadLine();
 
 
@@ -31,19 +43,27 @@ class InvoiceProcessor
     {
         Console.WriteLine(invoices.Count);
 
-        foreach (var invoice in invoices.GetConsumingEnumerable())
+        foreach (var invoice in invoices.GetConsumingEnumerable()) // To nie jest Pulling. To jest sygnał
         {
             Console.WriteLine($"{invoice} is accounting...");
             Thread.Sleep(5000);
             Console.WriteLine($"{invoice} is accounted.");
         }
+
+        Console.WriteLine("Koniec przetwarzania faktur");
     }
 
     public void Accounting(Invoice invoice)
     {
-        
+
         invoices.Add(invoice);
         Console.WriteLine($"Invoice {invoice} added");
+    }
+
+    public void Complete()
+    {
+       // invoices.CompleteAdding();
+        Console.WriteLine("Koniec przyjmowania faktur");
     }
 }
 
