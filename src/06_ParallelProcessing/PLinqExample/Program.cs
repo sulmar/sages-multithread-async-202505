698e2ba801ac8deac0fbc51a9a21b15389c94433
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-23 15:11:47 +0200

    Dodadnie przykładu synchronicznego

--- src/06_ParallelProcessing/PLinqExample/Program.cs
+++ src/06_ParallelProcessing/PLinqExample/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Diagnostics;
+
+Console.WriteLine("Hello, Linq!");
+
+
+var numbers = Enumerable.Range(1, 10_000);
+
+Stopwatch stopwatch = Stopwatch.StartNew();
+
+var primes = numbers
+    .Where(IsPrime)
+    .ToList();
+
+stopwatch.Stop();
+
+Console.WriteLine($"Executing time: {stopwatch.ElapsedMilliseconds} ms");
+Console.WriteLine($"Znaleziono {primes.Count} liczb pierwszych.");
+Console.WriteLine($"Pierwsze 10: {string.Join(", ", primes.Take(10))}");
+
+
+static bool IsPrime(int number)
+{
+    if (number <= 1) return false;
+    if (number == 2) return true;
+    if (number % 2 == 0) return false;
+
+    int boundary = (int)Math.Floor(Math.Sqrt(number));
+
+    for (int i = 3; i <= boundary; i += 2)
+    {
+        if (number % i == 0) return false;
+    }
+
+    return true;
+}
